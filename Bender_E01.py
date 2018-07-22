e4dabadf57deaedeccf110b22af4bb9477ae42de
Caio Martinelli <caio.martinelli@hotmail.com>
2018-07-22 20:23:50 -0300

    bender till levet 10

--- Bender_E01.py
+++ Bender_E01.py
@@ -1,13 +1,16 @@
 import sys
 import math
 
-global BREAKER, STATE, PRIORITIES, MOVES, MOVING
+global BREAKER, STATE, PRIORITIES, MOVES, MOVING, CHANGE_D, pos, CHANGE_P, Matrix
 BREAKER = False
+CHANGE_D = ''
+CHANGE_P = [-1,-1]
 STATE = 'R'
 PRIORITIES = {'R': ['SOUTH', 'EAST', 'NORTH', 'WEST'],
               'I': ['WEST', 'NORTH', 'EAST', 'SOUTH']}
-MOVES = {'SOUTH':(1,0), 'EAST':(0,1), 'NORTH':(-1,0), 'WEST':(-1,0)}
+MOVES = {'SOUTH':(1,0), 'EAST':(0,1), 'NORTH':(-1,0), 'WEST':(0,-1)}
 MOVING = 'SOUTH'
+
 
 l, c = [int(i) for i in input().split()]
 Matrix = [['0' for x in range(c)] for y in range(l)] 
@@ -28,7 +31,7 @@ def where(element):
     return answer
 
 def is_possible(move):
-    global BREAKER, STATE, PRIORITIES, MOVES, MOVING
+    global BREAKER, STATE, PRIORITIES, MOVES, MOVING, CHANGE_D, pos, CHANGE_P, Matrix
     block = Matrix[pos[0] + move[0]][pos[1] + move[1]]
     sub_dict = {'S': 'SOUTH','W': 'WEST','E': 'EAST','N': 'NORTH'}
     if block == 'B':
@@ -44,10 +47,20 @@ def is_possible(move):
             STATE = 'I'
             return True
     elif block in list(sub_dict.keys()):
-        MOVING = sub_dict[block]
+        CHANGE_D = sub_dict[block]
         return True
     elif block == 'X' and BREAKER:
+        Matrix[pos[0] + move[0]][pos[1] + move[1]] = ' '
         return True    
+    elif block == 'T':
+    	pos_ts = where('T')
+    	#print([pos[0] + move[0], pos[1] + move[1]], file=sys.stderr)
+    	#print(pos_ts[0], file=sys.stderr)
+    	if [pos[0] + move[0], pos[1] + move[1]] == pos_ts[0]:
+    		CHANGE_P = pos_ts[1]
+    	else:
+    		CHANGE_P = pos_ts[0]
+    	return True
     else: 
         MOVING = ''
         return False
@@ -58,7 +71,7 @@ pos = where('@')[0]
 print(f'Initial Position: {pos}', file=sys.stderr)
 posf = where('$')[0]
 print(f'Suicide Position: {posf}', file=sys.stderr)
-backtrack = []
+backtrack = set()
 loop_check = []
 
 print(len(MOVING), file=sys.stderr)
@@ -66,14 +79,21 @@ print(STATE, file=sys.stderr)
 print(PRIORITIES[STATE], file=sys.stderr)
 while pos != posf:
     while is_possible(MOVES[MOVING]):
+        #print(f'Moving: {MOVING}, Change_D: {CHANGE_D}, Change_P: {CHANGE_P}', file=sys.stderr)
         m = MOVES[MOVING]
-        #if (pos[0], pos[1], p, STATE, BREAKER, MOVING) in backtrack:
-        #    print('LOOP')
-        backtrack.append((pos[0], pos[1], MOVING, STATE, BREAKER))
-        print(backtrack, file=sys.stderr)
+        if (pos[0], pos[1], STATE, BREAKER, MOVING) in backtrack:
+            print('LOOP')
+        backtrack.add((pos[0], pos[1], MOVING, STATE, BREAKER))
+        #print(backtrack, file=sys.stderr)
         pos[0] += m[0]
         pos[1] += m[1]
-        print(pos, file=sys.stderr)
+        if CHANGE_D != '':
+        	MOVING = CHANGE_D
+        	CHANGE_D = ''
+        if CHANGE_P != [-1,-1]:
+        	pos = CHANGE_P
+        	CHANGE_P = [-1,-1]
+        #print(pos, file=sys.stderr)
         if pos == posf:
             for i in backtrack:
                 print(i[2])
@@ -82,16 +102,17 @@ while pos != posf:
 
     for p in PRIORITIES[STATE]:
         m = MOVES[p]
-        print(m, file=sys.stderr)
+        #print(m, file=sys.stderr)
         if is_possible(m):
-            #if (pos[0], pos[1], p, STATE, BREAKER, MOVING) in backtrack:
-            #    print('LOOP')
+            if (pos[0], pos[1], STATE, BREAKER, MOVING) in backtrack:
+                print('LOOP')
+                sys.exit()
             MOVING = p
-            backtrack.append((pos[0], pos[1], MOVING, STATE, BREAKER))
+            backtrack.add((pos[0], pos[1], MOVING, STATE, BREAKER))
             #print(backtrack[-1][2])
             pos[0] += m[0]
             pos[1] += m[1]
-            print(pos, file=sys.stderr)
+            #print(pos, file=sys.stderr)
             if pos == posf:
                 for i in backtrack:
                     print(i[2])
